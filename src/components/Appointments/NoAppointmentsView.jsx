95e7402980467f67f52a43a781e72c7792d9a72f
Brandon Dorner <brandon.dorner@anedot.com>
2023-03-02 15:12:51 -0600

    Create `NoAppointmentsView` component
    This component will be displayed when there are no appointments available.

---
+++ src/components/Appointments/NoAppointmentsView.jsx
@@ -0,0 +1,10 @@
+const NoAppointmentsView = () => {
+  return (
+    <div>
+      <h4>You Have No Appointments</h4>
+      <button>Add Appointment</button>
+    </div>
+  )
+}
+
+export default NoAppointmentsView
